69c760311719502f7f406adb329ed04c86eb7e8f
Adrien LEGRAND <legrand.ax@gmail.com>
2014-11-24 00:53:19 +0100

    difficulty fixed

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,7 +7,7 @@ function generateBoard(){
     for (var x = 0; x < size; x++) { 
         board[x] = [];
         for (var y = 0; y < size; y++) {
-        	board[x][y] = [Math.random()<.88 ? 0 : 1];
+        	board[x][y] = [Math.random()<.8 ? 0 : 1];
         }
     }
     board[0][0] = [0];								//1st case is the player's spawn, so unbombable.
